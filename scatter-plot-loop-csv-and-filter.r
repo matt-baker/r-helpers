df71bc54677ebb46a51fc93e7e42e1f7d44fef2a
Matt Baker <matt.baker@mspmac.org>
2013-06-03 17:21:11 -0400

    Updated field names to be more generic and fixed a typo

--- scatter-plot-loop-csv-and-filter.r
+++ scatter-plot-loop-csv-and-filter.r
@@ -1,4 +1,4 @@
-# Used to loop through csv files that contained x/x points coded by year.
+# Used to loop through csv files that contained x/y points coded by year and output scatter plot.
 # sqld is used in the loop to select each year
 
 # Setup
@@ -7,11 +7,11 @@ outputDir="I:/output/"
 fileExt = "csv"
 years <- c("2004","2008","2009","2010","2011","2012")
 
-# Libraries
+# Include
 library(sqldf)
 
-setwd(workingDir)
 # Get files in dir
+setwd(workingDir)
 files=list.files() #Could set to only filter CSV
 names <- substr(files,1,7)
 
@@ -19,10 +19,10 @@ names <- substr(files,1,7)
 for (i in 1:length(files)){
 
   # Set random colour for use in plot
-	random = sample(1:3,1)
-	if(random == 1){colour="#FF8C0032"} # Orange
-	else if (random == 2){colour="#00640032"} #Green
-	else if (random == 3) {colour="#FF303032"} #Red
+  random = sample(1:3,1)
+  if(random == 1){colour="#FF8C0032"} # Orange
+  else if (random == 2){colour="#00640032"} #Green
+  else if (random == 3) {colour="#FF303032"} #Red
 
   thisFile = paste(names[i],fileExt,sep="",collapse=NULL)	
 	csvData=read.csv(thisFile)
@@ -37,11 +37,11 @@ for (i in 1:length(files)){
 		outputFile=paste(outputDir,"g",i,"_",year,".png",sep="",collapse=NULL)		
 		png(filename=outputFile,width=800,height=800,units="px")		
 
-		# Generate output
-		x <- thisData$distance_mi
-		y <- thisData$altitude_ft
+		# Map x/y fields
+		x <- thisData$xField
+		y <- thisData$yField
 
-		plot(x,y,col=colour, pch=20, main=mapTitle,xlab="X axis label", ylab="Y alis label")
+		plot(x,y,col=colour, pch=20, main=mapTitle,xlab="X axis label", ylab="Y axis label")
 		dev.off()
 	}
 
